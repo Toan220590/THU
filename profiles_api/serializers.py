4fd294275098f73b1d6cd82f79c3110adb76d139
Toan220590 <quynhon.iot@gmail.com>
2023-03-14 07:50:35 +0700

    thrm profile user

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for our profile object."""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'last_login', 'password')
+        extra_kwargs = {
+            'last_login': {'read_only': True},
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        """Used to create a new user."""
+
+        user = models.UserProfile(
+            email=validated_data['email'],
+            name=validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -10,6 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -96,3 +97,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes =(permissions.UpdateOwnProfile,)
